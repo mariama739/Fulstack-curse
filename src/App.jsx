f8d45203606c3401494dd928c7b269db02dcc268
mariama739 <mariamacoly739@gmail.com>
2024-10-03 13:30:52 +0000

    partie1 A

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({name, exercises}) => {
+  console.log(name)
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({parts}) => {
+  console.log(parts);
+
+  return (
+    <div>
+      <Part name={parts.part1} exercises={parts.exercises1} />
+      <Part name={parts.part2} exercises={parts.exercises2} />
+      <Part name={parts.part3} exercises={parts.exercises3} />
+    </div>
+  );
+};
+
+
+const Total = ({ totalExercises }) => {
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -7,19 +38,20 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const parts = 
+    {
+    part1: part1, exercises1: exercises1,
+    part2: part2, exercises2: exercises2 ,
+    part3: part3, exercises3: exercises3 ,
+    }
+  
+  const totalExercises = exercises1 + exercises2 + exercises3;
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total totalExercises={totalExercises} />
     </div>
   );
 };
---
+++ src/part.jsx
@@ -0,0 +1,9 @@
+const Part = () => {
+  return (
+    <div>
+      <p>Hello moi</p>
+    </div>
+  );
+};
+
+export default Part;
